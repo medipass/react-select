39ecbe2b41b9ebe30c3e102163d5e3cbc9b851ef
jxom <jakemoxey@gmail.com>
2017-09-15 18:33:50 +1000

    build

--- dist/react-select.es.js
+++ dist/react-select.es.js
@@ -1878,88 +1878,6 @@ Select$1.defaultProps = {
 	valueKey: 'value'
 };
 
-/* global setTimeout, clearTimeout */
-
-function debounce(fn) {
-  var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-  var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-
-  var lastCallAt = void 0;
-  var deferred = void 0;
-  var timer = void 0;
-  var pendingArgs = [];
-  return function debounced() {
-    var currentWait = getWait(wait);
-    var currentTime = new Date().getTime();
-
-    var isCold = !lastCallAt || currentTime - lastCallAt > currentWait;
-
-    lastCallAt = currentTime;
-
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    if (isCold && options.leading) {
-      return options.accumulate ? fn.call(this, [args]).then(function (result) {
-        return result[0];
-      }) : fn.call.apply(fn, [this].concat(args));
-    }
-
-    if (deferred) {
-      clearTimeout(timer);
-    } else {
-      deferred = defer();
-    }
-
-    pendingArgs.push(args);
-    timer = setTimeout(flush.bind(this), currentWait);
-
-    if (options.accumulate) {
-      var argsIndex = pendingArgs.length - 1;
-      return deferred.promise.then(function (results) {
-        return results[argsIndex];
-      });
-    }
-
-    return deferred.promise;
-  };
-
-  function flush() {
-    var thisDeferred = deferred;
-    clearTimeout(timer);
-    if (options.accumulate) {
-      fn.call(this, pendingArgs).then(function (res) {
-        return thisDeferred.resolve(res);
-      }, function (err) {
-        return thisDeferred.reject(err);
-      });
-    } else {
-      fn.apply(this, pendingArgs[pendingArgs.length - 1]).then(function (res) {
-        return thisDeferred.resolve(res);
-      }, function (err) {
-        return thisDeferred.reject(err);
-      });
-    }
-
-    pendingArgs = [];
-    deferred = null;
-  }
-}
-
-function getWait(wait) {
-  return typeof wait === 'function' ? wait() : wait;
-}
-
-function defer() {
-  var deferred = {};
-  deferred.promise = new Promise(function (resolve, reject) {
-    deferred.resolve = resolve;
-    deferred.reject = reject;
-  });
-  return deferred;
-}
-
 var propTypes = {
 	autoload: PropTypes.bool.isRequired, // automatically call the `loadOptions` prop on-mount; defaults to true
 	cache: PropTypes.any, // object to use to cache results; set to null/false to disable caching
@@ -2191,6 +2109,8 @@ var Async = function (_Component) {
 	}, {
 		key: 'render',
 		value: function render() {
+			var _this3 = this;
+
 			var _props4 = this.props,
 			    children = _props4.children,
 			    loadingPlaceholder = _props4.loadingPlaceholder,
@@ -2204,12 +2124,15 @@ var Async = function (_Component) {
 			var props = {
 				noResultsText: this.noResultsText(),
 				placeholder: isLoading ? loadingPlaceholder : placeholder,
-				options: isLoading && loadingPlaceholder && !isLoadingPage ? [] : options
+				options: isLoading && loadingPlaceholder && !isLoadingPage ? [] : options,
+				ref: function ref(_ref) {
+					return _this3.select = _ref;
+				}
 			};
 
 			return children(_extends({}, this.props, props, {
 				isLoading: isLoading,
-				onInputChange: debounce(this._onInputChange, 500),
+				onInputChange: this._onInputChange,
 				onMenuScrollToBottom: this._onMenuScrollToBottom
 			}));
 		}
