3151ca203fc293cd9798d1007f53975d7b59f705
jxom <jakemoxey@gmail.com>
2017-09-15 18:44:07 +1000

    build

--- src/Async.js
+++ src/Async.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import debounce from './utils/debounce';
 import Select from './Select';
 import stripDiacritics from './utils/stripDiacritics';
 const propTypes = {
